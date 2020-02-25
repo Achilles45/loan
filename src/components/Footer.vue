18433399d44fefdd203d7c3ad184cd1a1047a4b3
Achilles45 <achillesusuoyibo7@gmail.com>
2020-02-25 08:09:11 -0800

    Added the correct details to the footer section

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,8 +7,8 @@
                     <div class="col-md-9">
                         <h4>Networth International</h4>
                         <p>Getting ahead financially requires you to be proactive and prudent in al your spendings. Networth International is your partner of choice that provides you with easy-to-read and real-time financial data so you can make informed investments decisions. Since inception, we have been committed to prodviding you with great products and customer services. Try us today!</p>
-                        <p><span class="office">Physical Office</span> 1 Adani Road, Off NYSC Camp, Enugu, Enugu State</p>
-                        <p class="office">08103515205(Mon-Fri From 9am-5pm)-support@virtualbanker.com</p>
+                        <p><span class="office">Physical Office</span> 245 East Ohio Street Chicago, IL 60611</p>
+                        <p class="office">+1 618-710-6254 (Mon-Fri From 9am-5pm)</p>
                         <small>&copy;Networth International. 2020. All rights reserved</small>
                     </div>
                     <div class="col-md-1"></div>
